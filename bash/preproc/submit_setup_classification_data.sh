e3e30c6d8bd51e2a594ec986985dbae0da5e28fb
sgonzalez122 <60163234+sgonzalez122@users.noreply.github.com>
2026-03-25 10:13:21 -0700

    Update submit_setup_classification_data.sh
    Changed directory path to mine
    Changed email

--- bash/preproc/submit_setup_classification_data.sh
+++ bash/preproc/submit_setup_classification_data.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=48:00:00
 #SBATCH -n 1
 #SBATCH --mem=8G
-#SBATCH --mail-user=ethanroy@stanford.edu
+#SBATCH --mail-user=sagon151@stanford.edu
 #SBATCH --mail-type=ALL
 #SBATCH --partition=normal,gse
 
 ml load matlab/R2023b 
 
 matlab -nodisplay < \
-/home/users/ethanroy/eeg_groupitizing/code/matlab/preproc/setup_classifcation_data_sherlock.m
+/home/users/sagon151/eeg-dot-digit-analysis/matlab/preproc/setup_classifcation_data_sherlock.m
